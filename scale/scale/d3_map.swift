e1468001a76d1f409bc897a6cccf8d6ceb1c4460
zeMirco <mirco.zeiss@gmail.com>
2014-07-09 19:59:07 +0200

    add methods to d3_map

--- scale/scale/d3_map.swift
+++ scale/scale/d3_map.swift
@@ -1,9 +1,38 @@
-//
-//  d3_map.swift
-//  scale
-//
-//  Created by Zeiss, Mirco on 07.07.14.
-//  Copyright (c) 2014 zemirco. All rights reserved.
-//
 
-import Foundation
+import UIKit
+
+class d3_Map {
+    
+    var d3_map_prefix = "\x00"
+    var store: Dictionary<String, NSNumber> = [:]
+    
+    init() {}
+    
+    /**
+     * Returns true if and only if this map has an entry for the specified key string. 
+     * Note: the value may be null or undefined.
+     */
+    func has(key: String) -> Bool? {
+        var value = self.store[key]
+        return value ? true : nil
+    }
+    
+    /**
+     * Returns the value for the specified key string. 
+     * If the map does not have an entry for the specified key, returns undefined.
+     */
+    func get(key: String) -> NSNumber? {
+        return self.store[key]
+    }
+    
+    /**
+     * Sets the value for the specified key string; returns the new value.
+     * If the map previously had an entry for the same key string, 
+     * the old entry is replaced with the new value.
+     */
+    func set(key: String, value: NSNumber) -> NSNumber {
+        self.store[key] = value
+        return value
+    }
+    
+}
